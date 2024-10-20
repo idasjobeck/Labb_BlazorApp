d4df46b440fe6822bbb008345de384437fed0c95
Ida Sjöbeck <ida.sjobeck@gmail.com>
2024-10-20 21:49:00 +0200

    Added form and functionality to New User page.

---
+++ Labb_BlazorApp/Components/Pages/NewUser.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace Labb_BlazorApp.Components.Pages;
+
+public partial class NewUser
+{
+    public bool displayForm = true;
+    private Users.User user = new Users.User();
+    private void AddUserToDb()
+    {
+        //would have code to add user to database.
+
+        //for the time being it just hides the form by setting displayForm to false.
+        displayForm = false;
+
+        Console.WriteLine("Data submitted successfully!");
+    }
+}
